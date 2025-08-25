127ae59a83479430da161a09d9c3bf0ddeabb11d
Ares Jilani <ares.jilani@elev.ga.ntig>
2025-08-25 12:50:39 +0200

    fdaf

--- Repition/Program.cs
+++ Repition/Program.cs
@@ -1,43 +1,51 @@
-﻿﻿FightSim();
+﻿using System.Threading.Tasks.Dataflow;
+
+FightSim();
 
 static void FightSim()
 {
-  int p1Hp = 100;
-  int p2Hp = 75;
+    int p1Hp = 100;
+    int p2Hp = 75;
 
-  string p1Name = "Micke";
-  string p2Name = "Martin";
+    string p1Name = "";
+    string p2Name = "Martin";
 
-  Console.WriteLine($"{p1Name} vs {p2Name} - FIGHT!");
+    Console.WriteLine("Write yo name fool");
+    
+    p1Name = Console.ReadLine();
 
-  while (p1Hp > 0 && p2Hp > 0)
-  {
-    Console.WriteLine($"{p1Name}: {p1Hp}");
-    Console.WriteLine($"{p2Name}: {p2Hp}");
+    
 
-    int p1Damage = Random.Shared.Next(25);
-    p2Hp -= p1Damage;
-    Console.WriteLine($"{p1Name} gör {p1Damage} i skada på {p2Name}");
+    Console.WriteLine($"{p1Name} vs {p2Name} - FIGHT!");
 
-    int p2Damage = Random.Shared.Next(25);
-    p1Hp -= p2Damage;
-    Console.WriteLine($"{p2Name} gör {p2Damage} i skada på {p1Name}");
+    while (p1Hp > 0 && p2Hp > 0)
+    {
+        Console.WriteLine($"{p1Name}: {p1Hp}");
+        Console.WriteLine($"{p2Name}: {p2Hp}");
+
+        int p1Damage = Random.Shared.Next(25);
+        p2Hp -= p1Damage;
+        Console.WriteLine($"{p1Name} gör {p1Damage} i skada på {p2Name}");
+
+        int p2Damage = Random.Shared.Next(25);
+        p1Hp -= p2Damage;
+        Console.WriteLine($"{p2Name} gör {p2Damage} i skada på {p1Name}");
+        Console.ReadLine();
+    }
+
+    if (p2Hp <= 0 && p1Hp <= 0)
+    {
+        Console.WriteLine($"{p1Name} och {p2Name} svimmade båda!");
+    }
+    else if (p1Hp <= 0)
+    {
+        Console.WriteLine($"{p2Name} vann!");
+    }
+    else
+    {
+        Console.WriteLine($"{p1Name} vann!");
+    }
+
+
     Console.ReadLine();
-  }
-
-  if (p2Hp <= 0 && p1Hp <= 0)
-  {
-    Console.WriteLine($"{p1Name} och {p2Name} svimmade båda!");
-  }
-  else if (p1Hp <= 0)
-  {
-    Console.WriteLine($"{p2Name} vann!");
-  }
-  else
-  {
-    Console.WriteLine($"{p1Name} vann!");
-  }
-
-
-  Console.ReadLine();
 }
